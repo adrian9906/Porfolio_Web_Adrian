80292f8e67341fff0d796083b9087c38a659c53f
adrian9906 <adriandfl99@gmail.com>
2023-12-31 09:11:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Adrian Fernández Landivar | Website';
+export const SITE_DESCRIPTION = 'Adrian Fernández Landivar personal website template with Project Section, Resume Section, Visual themes and Responsive Desing.';
